ae96b8e6c4b471af8cc4f660c37741d25d6dc609
prajwalbutte <prajwaldbutte@gmail.com>
2026-09-07 20:55:06 +0530

    Arrays revison

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Practice {
     public static void main(String[] args) {
 
-        int [] arr = {1,3,4,5};
+        int [] arr = {1,2,4,5};
 
         // int sum = Arrays.stream(arr).sum();
         // System.out.println(sum);
@@ -14,21 +14,83 @@ public class Practice {
         // }
         // System.out.println(product);
 
+        // int max = arr[0];
+        // int min = arr[0];
+        // System.out.println(max);
+
+        // for ( int i = 0; i < arr.length; i++) {
+        //     if(arr[i] > max){
+        //         max = arr[i];
+        //     }
+        //     if(arr[i] < min){
+        //         min = arr[i];
+
+        //     }
+        // }
+        // System.out.println(max);
+        // System.out.println(min);
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     if(i % 2 == 0){
+        //         arr[i] *= 10 ;
+        //     }
+        //     if(i % 2 != 0){
+        //         arr[i] *= 2;
+        //     }
+
+        // }
+        // for (int i : arr) {
+        //     System.out.print(i+" ");
+        // }
+
+        // int target = 3;
+        // int found = -1;
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (arr[i] == target) {
+        //         found = 1;
+        //         break;
+        //     }
+            
+        // }
+
+        // if(found == 1){
+        //     System.out.println("Found");
+        // }
+        // else{
+        //     System.out.println("Not found");
+        // }
+
+        // int sum = 3;
+        // for (int i = 0; i < arr.length; i++) {
+        //     for(int j = i+1;j<arr.length-1;j++){
+        //         if(arr[i]+arr[j] == sum){
+        //             System.out.println(i+" "+j);
+        //         }
+        //     }
+        // }
+
         int max = arr[0];
-        int min = arr[0];
-        System.out.println(max);
+int secondMax = Integer.MIN_VALUE;
 
-        for ( int i = 0; i < arr.length; i++) {
-            if(arr[i] > max){
-                max = arr[i];
-            }
-            if(arr[i] < min){
-                min = arr[i];
+// Find maximum
+for (int i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
 
-            }
-        }
-        System.out.println(max);
-        System.out.println(min);
+// Find second maximum
+for (int i = 0; i < arr.length; i++) {
+    if (arr[i] > secondMax && arr[i] != max) {
+        secondMax = arr[i];
+    }
+}
+
+System.out.println(secondMax);
+    
+
+
 
         
 
