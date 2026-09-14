20c1c40a96d6c6cd9d76b4863a6e444a9cb87712
prajwalbutte <prajwaldbutte@gmail.com>
2026-09-14 16:12:29 +0530

    Kth Smallest Element Basic Approach

---
+++ Sorting/KthSmallestBasic.java
@@ -0,0 +1,16 @@
+package Sorting;
+import java.util.*;
+
+public class KthSmallestBasic {
+    public static void main(String[] args) {
+        
+        int []a = {7,10,4,3,20,15};
+        int k = 3;
+
+        Arrays.sort(a);
+
+         System.out.print(a[k-1]);
+        
+    }
+    
+}
